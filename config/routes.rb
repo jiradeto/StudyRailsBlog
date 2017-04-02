5d4eaddf7271e5ac046954a3738b35bf0c4373bc
jiradeto <jiradet.jd@gmail.com>
2017-04-02 17:20:02 +0700

    add category controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+
+  get 'categories/show'
+
+  get 'categories/edit'
+
+  get 'categories/new'
+
   resources :posts
+  resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
