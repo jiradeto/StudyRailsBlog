ed3ea18d19836e1cfbef257656029f3fe8ad97fa
jiradeto <jiradet.jd@gmail.com>
2017-04-03 02:06:48 +0700

    upadate partial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :all_categories
+
+  def all_categories
+    Category.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
+
+  get 'about' => 'pages#about'
+  get 'contact' => 'pages#contact'
+  get 'resources' => 'pages#resources'
+
+
+
+  ActiveAdmin.routes(self)
   resources :cars
-  get 'categories/index'
-
-  get 'categories/show'
-
-  get 'categories/edit'
-
-  get 'categories/new'
-
   resources :posts
   resources :categories
+  root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
